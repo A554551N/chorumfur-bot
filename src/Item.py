1809a72e24713ed0cf2938b799b6f2c477e588c6
A554551N <linuxfreak1030@gmail.com>
2022-11-10 12:39:28 -0500

    updated Docstring

--- src/Item.py
+++ src/Item.py
@@ -1,4 +1,21 @@
 class Item:
+    """
+    A class representing an individual Item
+
+    Attributes
+    ----------
+    name : string
+        An item's friendly name
+    description : string
+        An item's description
+    value : int
+        Item's currency value
+    imageLink : string
+        URL to image
+    id : int
+        ID number in Database
+    """
+    
     def __init__(self,name,description,value,imageLink,id=None):
         self.name = name
         self.description = description
