b4a95968b88307d80794a9ea56c0b542ad2e7987
A554551N <linuxfreak1030@gmail.com>
2022-11-16 16:44:06 -0500

    wrote decorator for connecting to DB

---
+++ src/database_methods.py
@@ -0,0 +1,26 @@
+"""Contains methods for interacting with the backend database
+Methods
+-------
+database_connection(func)
+    Decorator that safely constructs and destructs the database
+"""
+import psycopg2
+
+def database_connection(func):
+    def inner(*args,**kwargs):
+        try:
+            conn = psycopg2.connect(
+                host="localhost",
+                database="chorumfur",
+                user="postgres",
+                password="postgres"
+            )
+            output = func(conn,*args,**kwargs)
+        except psycopg2.DatabaseError as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close
+                print("DB Closed")
+        return output
+    return inner
