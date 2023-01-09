15955d7fdaa4f42db49706226d529b1e4f5615c3
A554551N <linuxfreak1030@gmail.com>
2023-01-09 11:40:44 -0500

    removed redundant tests

--- tests/test_User.py
+++ tests/test_User.py
@@ -81,42 +81,9 @@ def test_breedingLevelImage(userAttributes):
                         userAttributes["warningsIssued"])
     assert "https://media.discordapp.net/attachments/1039966957799211109/1039967098174185552/Breeding_Crystal.png" == User.BREEDINGSTONELINKS[testUser.breedingLevel(True)]
 
-# DB Read/Write Tests
-
-def test_add_new_user_to_db(userAttributes):
-    """Attempts to add a user to the database and asserts that it completed successfully"""
-    assert database_methods.add_user_to_database(userAttributes["ID"])
-
-
-def test_reject_existing_user_in_db(createTestUser):
-    """Asserts that duplicate users are not added to database"""
-    createTestUser.userId = 99999
-    assert not database_methods.add_user_to_database(createTestUser.userId)
-
-def test_get_user_from_db():
-    """asserts that a user can be retreived from the database"""
-    returnedUser = database_methods.get_user_from_db(99999)
-    assertValues = (returnedUser.userId,returnedUser.level)
-    assert assertValues == (99999,99)
-
 # Mothballing these tests while code is refactored.
-
-def test_addToUserInventory():
-    assert Database.addToUserInventory(99999,800,True)
-
-def test_getInventory(createTestUser):
-    createTestUser.userId = 99998
-    testUser = Database.getUserInventory(createTestUser,True)
-    assert testUser.inventory[1][1] == 2
 
 def test_emptyInventoryReturnsNone(createTestUser):
     createTestUser.userId = 123
     testUser = Database.getUserInventory(createTestUser,True)
     assert not testUser.inventory
-
-def test_removeItemFromInventory(createTestUser):
-    createTestUser.userId = 99999
-    Database.addToUserInventory(99999,1000,True)
-    Database.removeFromUserInventory(99999,1000,True)
-    testUser = Database.getUserInventory(createTestUser,True)
-    assert 1000 not in testUser.inventory.keys()
