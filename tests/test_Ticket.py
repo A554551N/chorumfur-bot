e6a6d22fe05854ab7569ebac45f7f91c23dfd720
A554551N <linuxfreak1030@gmail.com>
2023-01-23 11:37:57 -0500

    tests to support update_ticket_status

--- tests/test_Ticket.py
+++ tests/test_Ticket.py
@@ -47,12 +47,25 @@ def test_add_ticket_to_db():
 
 def test_get_ticket_from_db():
     """Test should retreive a ticket from the database and return its ID"""
-    test_ticket = database_methods.get_ticket_from_db(1)
-    expected_result = (1,27,28,202632427535859712)
+    test_ticket = database_methods.get_ticket_from_db(40)
+    expected_result = (40,167,168,202632427535859712)
     returned_value = (
         test_ticket.id,
         test_ticket.creature_a.creatureId,
         test_ticket.creature_b.creatureId,
         test_ticket.requestor.userId)
     assert expected_result == returned_value
+
+def test_update_ticket_status():
+    """Test should update a ticket status, and then retreive it to confirm update."""
+    creature_a = database_methods.get_creature_from_db(167)
+    creature_b = database_methods.get_creature_from_db(168)
+    user = database_methods.get_user_from_db(202632427535859712)
+    user.lastBreed = datetime(2022,1,1)
+    test_ticket = Ticket("Test Ticket",user,creature_a,creature_b)
+    test_ticket.id = database_methods.add_ticket_to_db(test_ticket)
+    test_ticket.update_ticket_status(4)
+    database_methods.update_ticket_status(test_ticket)
+    returned_ticket = database_methods.get_ticket_from_db(test_ticket.id)
+    assert returned_ticket.status == 'Breeding in Progress'
 
