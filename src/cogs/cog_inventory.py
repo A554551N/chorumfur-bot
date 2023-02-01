7e2f43f979f7fd8ce454693ec32b646f723c4bf9
A554551N <linuxfreak1030@gmail.com>
2023-02-01 11:54:14 -0500

    added commands

--- src/cogs/cog_inventory.py
+++ src/cogs/cog_inventory.py
@@ -6,5 +6,22 @@ class InventoryCog(commands.GroupCog, name='Inventory Management',group_name='in
     def __init__(self,bot):
         self.client = bot
 
+    @commands.command()
+    async def getItem(self,ctx,item_id):
+        """Displays details of a given Item ID"""
+        item=database_methods.get_item_from_db(item_id)
+        await ctx.send(f"{ctx.message.author.mention}\n{item.outputItem()}")
+        if item.imageLink is not None:
+            await ctx.send(item.imageLink)
+    
+    @commands.command()
+    async def inventory(self,ctx):
+        """Displays a user's inventory"""
+        await ctx.send(f"Fetching Inventory {ctx.message.author.mention}")
+        user = database_methods.get_user_from_db(ctx.message.author.id)
+        await ctx.send(user.outputInventory())
+        await ctx.send("For more information on an item, use .getItem <ID Number>")
+
+
 async def setup(bot):
     await bot.add_cog(InventoryCog(bot))
