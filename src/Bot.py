c20da57fdeea9136f54aaec03d38753d1b53e98f
A554551N <linuxfreak1030@gmail.com>
2023-02-01 12:18:59 -0500

    refactor: moved breed functions to Cog

--- src/Bot.py
+++ src/Bot.py
@@ -251,56 +251,6 @@ async def getItem(ctx,item_id):
     if item.imageLink != "":
         await ctx.send(item.imageLink)
 
-@client.command()
-async def breed(ctx,creature_a_id,creature_b_id):
-    """Submit a breeding request in format .breed <creature_a> <creature_b>"""
-    breed_request=create_breeding_ticket(requesting_user_id=ctx.message.author.id,
-                                        creature_a_id=creature_a_id,
-                                        creature_b_id=creature_b_id)
-    if breed_request.requestor_can_breed():
-        if breed_request.requestor_owns_both():
-            breed_request = enact_breeding(breed_request)
-            breed_request.id = database_methods.add_ticket_to_db(breed_request)
-            await send_ticket_to_channel(breed_request)
-            await ctx.send("Breeding has been successfully submitted.  "\
-                          f"Your Ticket # is {breed_request.id}")
-        else:
-            await pend_breeding(ctx,breed_request)
-    else:
-        await ctx.send("Breeding request was not able to be submitted at this time."\
-                " Please confirm you own at least one of the creatures submitted "\
-                "and that your breeding crystal is fully charged.")
-
-@client.command(aliases=['accept'])
-async def acceptBreeding(ctx,ticket_id):
-    """.acceptBreeding <ticket_id> moves a ticket from pending state to active and
-    performs the breeding."""
-    ticket = database_methods.get_ticket_from_db(ticket_id)
-    if ticket.other_user() != ctx.message.author.id:
-        msg = "You do not have permission to modify this ticket."
-    elif ticket.status != Constants.TICKET_STATUS[2]:
-        msg = f"This ticket is in {ticket.status} and cannot be modified."
-    else:
-        ticket = enact_breeding(ticket)
-        database_methods.update_ticket_in_db(ticket)
-        await send_ticket_to_channel(ticket)
-        msg = f"Ticket {ticket.id} has been accepted.  Status is now {ticket.status}"
-    await ctx.send(msg)
-
-@client.command(aliases=['decline'])
-async def declineBreeding(ctx,ticket_id):
-    """.declineBreeding <ticket_id> moves a ticket from pending state to cancelled."""
-    ticket = database_methods.get_ticket_from_db(ticket_id)
-    if ticket.other_user() != ctx.message.author.id:
-        msg = "You do not have permission to modify this ticket."
-    elif ticket.status != Constants.TICKET_STATUS[2]:
-        msg = f"This ticket is in {ticket.status} and cannot be modified."
-    else:
-        ticket.update_ticket_status(6)
-        database_methods.update_ticket_in_db(ticket)
-        msg = f"Ticket {ticket.id} has been rejected.  Status is now {ticket.status}"
-    await ctx.send(msg)
-
 @is_guild_owner_or_me()
 @client.command(aliases=['gdt'])
 async def getDetailedTicket(ctx,ticket_id):
