39eedd7af14f355d21fd9224d4d0dc248814e9a8
A554551N <linuxfreak1030@gmail.com>
2023-03-03 09:45:41 -0500

    FEATURE: begin implementation of limited time events

--- src/cogs/cog_activities.py
+++ src/cogs/cog_activities.py
@@ -1,9 +1,10 @@
-from random import random
+import random
 from decimal import Decimal
 from discord.ext import commands
 import database_methods
 import support_functions
 import forage_outcomes
+import limited_time_events
 
 class ActivitiesCog(commands.GroupCog, name='Activities', group_name='activities'):
     """Cog to group commands related to activities for players"""
@@ -24,7 +25,37 @@ class ActivitiesCog(commands.GroupCog, name='Activities', group_name='activities
             subtype = support_functions.roll_random_result(forage_outcomes.outcome_types)
             possible_outcomes = forage_outcomes.outcome_subtypes[subtype]
             outcome = support_functions.roll_random_result(possible_outcomes)
-            await ctx.send(f"{creature.name} is foraging!\n{outcome.text}\n"\
+            msg = ""
+            if outcome.type == 'event_curr':
+                msg = outcome.text.format(limited_time_events.march_event.event_currency_name)
+                database_methods.add_item_to_user(ctx.author.id,limited_time_events.march_event.event_currency_id)
+                
+            elif outcome.type == 'lure':
+                msg = outcome.text
+                if outcome.reward is not None:
+                    msg += f'\nChorumfur #{outcome.reward} has decided to join your lair!'
+                    lured_chorumfur = database_methods.get_creature_from_db(outcome.reward)
+                    lured_chorumfur.owner = ctx.author.id
+                    database_methods.update_creature(lured_chorumfur)
+                else:
+                    wild_chorumfurs = database_methods.get_wild_chorumfur_ids()
+                    if not wild_chorumfurs:
+                        msg += "\nIt ran away, though."
+                    else:
+                        random_selection = random.randint(0,len(wild_chorumfurs)-1)
+                        chorumfur_to_add = database_methods.get_creature_from_db(wild_chorumfurs[random_selection][0])
+                        chorumfur_to_add.owner = ctx.author.id
+                        database_methods.update_creature(chorumfur_to_add)
+                        msg += f'\nChorumfur #{chorumfur_to_add.creatureId} has decided to join your lair!'
+
+            elif outcome.type == 'currency':
+                msg=outcome.text.format(outcome.reward)
+                # ADD A CURRENCY MANIP METHOD IN DATABASE_METHODS
+
+            elif outcome.type == 'text':
+                msg = outcome.text.format(creature.name)
+            await ctx.send(f"{creature.name} is foraging!\n{msg}")
+            await ctx.send(f"---DEBUG---\n{outcome.text}\n"\
                            f"Event Type: {outcome.type}\n"\
                             f"Reward: {outcome.reward}")
 
