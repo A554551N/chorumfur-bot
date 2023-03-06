901ac878a03bd452cccb29a66bf082db7de5fab8
A554551N <linuxfreak1030@gmail.com>
2023-03-05 22:50:25 -0500

    FIX: uncommented lines rate-limiting forage

--- src/cogs/cog_activities.py
+++ src/cogs/cog_activities.py
@@ -86,10 +86,10 @@ class ActivitiesCog(commands.GroupCog, name='Activities', group_name='activities
         elif not creature.is_active:
             msg = "Only the chorumfurs in your party can forage."
             valid = False
-        #elif creature.last_forage and (datetime.today() - creature.last_forage).seconds//3600 < 6:
-            #msg = "Each chorumfur can only forage once every six hours.  "\
-            #      f"You can forage again in {6 - (datetime.today() - creature.last_forage).seconds//3600} hours,"
-            #valid = False
+        elif creature.last_forage and (datetime.today() - creature.last_forage).seconds//3600 < 6:
+            msg = "Each chorumfur can only forage once every six hours.  "\
+                  f"You can forage again in {6 - (datetime.today() - creature.last_forage).seconds//3600} hours,"
+            valid = False
         return (valid,msg)
     
 async def setup(bot):
