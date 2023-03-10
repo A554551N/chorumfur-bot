ed9ffed272c087d70dfd252698c3d4062cb927c8
A554551N <linuxfreak1030@gmail.com>
2023-03-09 20:46:16 -0500

    update version number

--- src/ConstantData.py
+++ src/ConstantData.py
@@ -1,7 +1,7 @@
 class Constants:
     DATETIMEFORMAT = '''%Y-%m-%d %H:%M:%S.%f'''
     DATEONLYFORMAT = '%Y-%m-%d'
-    VERSION = 'v0.3.8'
+    VERSION = 'v0.3.9'
     DEFAULT_TRAITS_DICT = {
         'MAIN_HORN': "",
         'CHEEK_HORN': "",
